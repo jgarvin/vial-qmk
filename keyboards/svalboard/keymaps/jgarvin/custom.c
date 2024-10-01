ad4b42373d056d141b5e8560f2396d74de87aa83
Joseph Garvin <joseph.h.garvin@gmail.com>
2024-10-01 12:36:25 -0500

    finish quicktap keys

--- keyboards/svalboard/keymaps/jgarvin/custom.c
+++ keyboards/svalboard/keymaps/jgarvin/custom.c
@@ -19,65 +19,100 @@ uint8_t combo_ref_from_layer(uint8_t layer) {
     return 0;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    printf("LT3_SCROLL_LEFT_TOGGLE keycode: %04x\n", LT3_SCROLL_LEFT_TOGGLE);
+// Vial doesn't let you define LT(kc) keys that call custom keys like
+// the scroll toggles that sval has. If you try to implement the
+// equivalent with a tap dance key then you have to hold the key long
+// enough for qmk to be sure it's a hold, even though as soon as you
+// press another key while holding it's unambiguous you intend to use
+// the layer switch functionality.
+typedef struct quicktap {
+    uint16_t trigger_keycode;
+    uint16_t standalone_tap_keycode;
+    int layer;
+    bool pressed;
+    bool anything_else_pressed_while_holding;
+} quicktap_t;
 
-    static bool pressed = false;
-    static bool anything_else_pressed_while_holding = false;
-    static keyrecord_t left_scroll_record;
+quicktap_t quicktaps[2];
+
+// for now using pre init since the sval keymap.c uses post init
+void keyboard_pre_init_user(void)
+{
+    quicktaps[0].trigger_keycode = LT3_SCROLL_LEFT_TOGGLE;
+    quicktaps[0].standalone_tap_keycode = SV_LEFT_SCROLL_TOGGLE;
+    quicktaps[0].layer = 3;
+    quicktaps[1].trigger_keycode = LT4_SCROLL_RIGHT_TOGGLE;
+    quicktaps[1].standalone_tap_keycode = SV_RIGHT_SCROLL_TOGGLE;
+    quicktaps[1].layer = 4;
+
+    printf("LT3_SCROLL_LEFT_TOGGLE keycode: %04x\n", LT3_SCROLL_LEFT_TOGGLE);
+    printf("LT4_SCROLL_RIGHT_TOGGLE keycode: %04x\n", LT4_SCROLL_RIGHT_TOGGLE);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static bool recursing = false;
     if(recursing) {
         return true;
     }
 
-    print("===============================================\n");
-    printf("keycode %d\n", keycode);
-    printf("record->event.pressed %d\n", record->event.pressed);
-    printf("old_anything_else_pressed_while_holding %d\n", anything_else_pressed_while_holding);
-    bool old_pressed = pressed;
-    printf("old_pressed %d\n", pressed);
-    if(keycode == LT3_SCROLL_LEFT_TOGGLE)
+    for(int i = 0; i < sizeof(quicktaps)/sizeof(quicktaps[0]); i++)
     {
-        pressed = record->event.pressed;
-        printf("pressed %d\n", pressed);
-        if(old_pressed)
+        quicktap_t* qt = &quicktaps[i];
+
+        if(i == 0)
         {
-            if(!record->event.pressed)
+            print("===============================================\n");
+            printf("keycode %d\n", keycode);
+            printf("record->event.pressed %d\n", record->event.pressed);
+            printf("old_anything_else_pressed_while_holding %d\n", qt->anything_else_pressed_while_holding);
+            printf("old_pressed %d\n", qt->pressed);
+        }
+
+        bool old_pressed = qt->pressed;
+        if(keycode == qt->trigger_keycode)
+        {
+            qt->pressed = record->event.pressed;
+            printf("pressed %d\n", qt->pressed);
+            if(old_pressed)
             {
-                if(anything_else_pressed_while_holding)
+                if(!record->event.pressed)
                 {
-                    print("layer 3 off\n");
-                    anything_else_pressed_while_holding = false;
-                    printf("anything_else_pressed_while_holding %d\n", anything_else_pressed_while_holding);
-                    layer_off(3);
-                }
-                else
-                {
-                    print("tapping left scroll toggle\n");
-                    left_scroll_record         = *record;
-                    left_scroll_record.keycode = SV_LEFT_SCROLL_TOGGLE;
-                     left_scroll_record.event.pressed = true;
-                    recursing = true;
-                    print("recursing to allow through other key while this key is held")
-                    process_record(&left_scroll_record);
-                    recursing = false;
-                    left_scroll_record.event.pressed = false;
-                    return false;
+                    if(qt->anything_else_pressed_while_holding)
+                    {
+                        print("layer 3 off\n");
+                        qt->anything_else_pressed_while_holding = false;
+                        printf("anything_else_pressed_while_holding %d\n", qt->anything_else_pressed_while_holding);
+                        layer_off(3);
+                    }
+                    else
+                    {
+                        print("tapping left scroll toggle\n");
+                        keyrecord_t tap_record = *record;
+                        tap_record.keycode = qt->standalone_tap_keycode;
+                        print("recursing to allow through other key while this key is held");
+                        recursing = true;
+                        tap_record.event.pressed = true;
+                        process_record(&tap_record);
+                        tap_record.event.pressed = false;
+                        process_record(&tap_record);
+                        recursing = false;
+                        return false;
+                    }
                 }
             }
         }
-    }
-    else if(old_pressed && record->event.pressed && !anything_else_pressed_while_holding)
-    {
-        print("layer 3 on\n");
-        layer_on(3);
-        anything_else_pressed_while_holding = true;
-        printf("anything_else_pressed_while_holding %d\n", anything_else_pressed_while_holding);
-        recursing = true;
-        print("recursing to allow through other key while this key is held");
-        process_record(record);
-        recursing = false;
-        return false;
+        else if(old_pressed && record->event.pressed && !qt->anything_else_pressed_while_holding)
+        {
+            print("layer 3 on\n");
+            layer_on(3);
+            qt->anything_else_pressed_while_holding = true;
+            printf("anything_else_pressed_while_holding %d\n", qt->anything_else_pressed_while_holding);
+            print("recursing to allow through other key while this key is held");
+            recursing = true;
+            process_record(record);
+            recursing = false;
+            return false;
+        }
     }
 
     return true;
